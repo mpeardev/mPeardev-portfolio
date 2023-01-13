5945fa2593a5fff7a76392ac137fdcdd618c58cd
mirkoperamas <mirkoperamas@gmail.com>
2023-01-13 14:29:10 -0500

    update v1

--- src/components/molecules/home/ButtonsHome.jsx
+++ src/components/molecules/home/ButtonsHome.jsx
@@ -2,6 +2,18 @@ import { PrimaryButton, SecondaryButton } from "../../../components";
 import classes from "../../organims/Home/home.module.scss";
 
 export function ButtonsHome({ breakpoint, openComingModal }) {
+  const onPDFButtonClick = () => {
+    fetch("/pdf/Mirko_Peramas_CV.pdf").then((response) => {
+      response.blob().then((blob) => {
+        const fileURL = window.URL.createObjectURL(blob);
+        let alink = document.createElement("a");
+        alink.href = fileURL;
+        alink.download = "/Mirko_Peramas_CV.pdf";
+        alink.click();
+      });
+    });
+  };
+
   return (
     <>
       <div className={classes.home__aboutButtons}>
@@ -16,7 +28,7 @@ export function ButtonsHome({ breakpoint, openComingModal }) {
           <SecondaryButton
             name="Descargar CV"
             size={breakpoint >= 1280 ? "xl" : breakpoint >= 768 ? "lg" : "sm"}
-            onClick={openComingModal}
+            onClick={onPDFButtonClick}
           />
         </div>
       </div>
